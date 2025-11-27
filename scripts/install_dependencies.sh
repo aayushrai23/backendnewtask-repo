13353008c29e75255b5fde018aca490ba4e8b4cc
root <root@ip-10-0-2-202.ap-southeast-1.compute.internal>
2025-11-27 11:31:06 +0000

    Files are changed

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,10 @@
 set -e
 
 echo "Installing dependencies..."
+
+# Fix folder permissions before npm install
+sudo chown -R ubuntu:ubuntu /var/www/html/backend
+
 cd /var/www/html/backend
 
 npm install --legacy-peer-deps
